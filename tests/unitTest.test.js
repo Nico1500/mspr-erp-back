f91923dd5760a902e11452e9f379a21946ed5072
Zaki nou <zakidenimes@hotmail.fr>
2023-03-07 11:59:34 +0100

    test

--- tests/unitTest.test.js
+++ tests/unitTest.test.js
@@ -1,110 +1,45 @@
-import sinon from 'sinon';
-import expect from 'chai'
-import getUsers from '../src/controllers/users.controller.js'
-import pool from '../src/database.js'
+import { getUsers } from "../src/controllers/users.controller.js";
+import { pool } from "../src/database.js";
+import { afterAll } from 'jest';
 
 
-describe('getUsers', () => {
-  let poolQueryStub;
-  let res;
 
-  before(() => {
-    poolQueryStub = sinon.stub(pool, 'query');
+describe("getUsers", () => {
+  afterAll(async () => {
+    await pool.end();
   });
 
-  after(() => {
-    poolQueryStub.restore();
-  });
-
-  beforeEach(() => {
-    res = {
-      json: sinon.spy(),
-    };
-  });
-
-  afterEach(() => {
-    poolQueryStub.reset();
-    res.json.resetHistory();
-  });
-
-  it('should return the list of users', async () => {
+  test("should return all users from the database", async () => {
     // Arrange
-    const mockUsers = [
-      { id: 1, name: 'Alice' },
-      { id: 2, name: 'Bob' },
-      { id: 3, name: 'Charlie' },
-    ];
-
-    poolQueryStub.resolves({ rows: mockUsers });
+    const req = {};
+    const res = { json: jest.fn() };
 
     // Act
-    await getUsers(null, res);
+    await getUsers(req, res);
 
     // Assert
-    sinon.assert.calledWith(poolQueryStub, 'SELECT * FROM users');
-    sinon.assert.calledWith(res.json, mockUsers);
+    expect(res.json).toHaveBeenCalled();
   });
 
- 
+  test("should log an error if the database query fails", async () => {
+    // Arrange
+    const req = {};
+    const res = { json: jest.fn() };
+    const expectedError = new Error("Test error");
+
+    // Mock the pool.query function to throw an error
+    pool.query.mockImplementationOnce(() => {
+      throw expectedError;
+    });
+
+    console.error = jest.fn();
+
+    // Act
+    await getUsers(req, res);
+
+    // Assert
+    expect(console.error).toHaveBeenCalledWith(expectedError);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: "Internal server error" });
+  });
 });
-
-
-describe('GET /customers/:customerId/:apiKey', () => {
-  let poolQueryStub;
-
-  before(() => {
-    poolQueryStub = sinon.stub();
-    const pool = {
-      query: poolQueryStub,
-      connect: sinon.stub().resolves(),
-    };
-    Object.defineProperty(require('../src/database'), 'pool', { value: pool });
-  });
-
-  beforeEach(() => {
-    poolQueryStub.reset();
-  });
-
-  after(async () => {
-    await pool.query('DELETE FROM users');
-  });
-
-  it('should return the list of orders for a customer when a valid apiKey is provided', async () => {
-    // Arrange
-    const mockOrders = [
-      { id: 1, customerId: 1, totalAmount: 50.99 },
-      { id: 2, customerId: 1, totalAmount: 100.25 },
-      { id: 3, customerId: 1, totalAmount: 75.50 },
-    ];
-
-    poolQueryStub
-      .withArgs(`SELECT api_key FROM users WHERE api_key = 'validApiKey' UNION SELECT key FROM webshopkey WHERE key = 'validApiKey'`)
-      .resolves({ rowCount: 1 });
-
-    sinon.stub(getCustomerOrders, 'resolves').returns(mockOrders);
-
-    // Act
-    const response = await request(app).get('/customers/1/validApiKey');
-
-    // Assert
-    assert.strictEqual(response.status, 200);
-    assert.deepStrictEqual(response.body, mockOrders);
-  });
-
-  it('should return a 401 error when an invalid apiKey is provided', async () => {
-    // Arrange
-    poolQueryStub
-      .withArgs(`SELECT api_key FROM users WHERE api_key = 'invalidApiKey' UNION SELECT key FROM webshopkey WHERE key = 'invalidApiKey'`)
-      .resolves({ rowCount: 0 });
-
-    // Act
-    const response = await request(app).get('/customers/1/invalidApiKey');
-
-    // Assert
-    assert.strictEqual(response.status, 401);
-    assert.deepStrictEqual(response.body, { message: 'Accès refusé. Clé API invalide.' });
-  });
-
-});
-
-
