682adaa8af54328a2b8ee42bdf2720d6aa4c5763
nicolas cammareri <nicolas.cammareri.nc@gmail.com>
2023-03-08 09:33:03 +0100

    new version

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { pool } from './database.js';
 import router from './routes/Index.js';
 
 
@@ -9,6 +10,19 @@ app.use('/', router);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+});
+
+app.get('/test', async (req, res) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        return res.json(result.rows);
+      } catch (error) {
+        console.log(
+          "🚀 ~ file: users.controller.js ~ line 9 ~ getUsers ~ error",
+          error
+        );
+      }
+    res.send('c vu')
 });
 
 app.listen(3000, () => console.log(`App listening at port 3000`));
